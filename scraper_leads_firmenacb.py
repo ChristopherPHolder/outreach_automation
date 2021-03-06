81d8f8f2fb9b838c7b1dad96c6eec7cfaeda5309
Christopher <toholderandrews@gmail.com>
2021-03-06 14:23:36 -0600

    Extracted the user input funtions structured the code so that it can be called as a funtion

--- scraper_leads_firmenacb.py
+++ scraper_leads_firmenacb.py
@@ -6,214 +6,221 @@ from tqdm import tqdm  # Progress bar / counter
 import pandas as pd  # Library to store and export data formated as a table
 import time  # Library to allow waiting and sleep
 
-# Request input from user for company type and location
-companyName = input("Enter the company type: ")
-location = input("Enter the location (Bezirk/Ort/Plz): ")
-# limit = 100  # porcentaje de extraccion total - 100%
-limit = int(input("Enter the percentage (number 0-100) of results to export: "))
+from comp.get_user_input import get_company_type, get_company_location, get_search_limit
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("https://www.firmenabc.at/")
+def scrape_firmenabc():
+    # Request input from user for company type, location and search limit
+    companytype = get_company_type()
+    location = get_company_location()
+    limit = get_search_limit()
 
-# Wait for cookie popup to load
-time.sleep(5)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get("https://www.firmenabc.at/")
 
-# Accept cookie popup
-cookiesHandle = driver.find_element_by_xpath(
-    '//*[@id="CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"]'
-)
-cookiesHandle.click()
+    # Wait for cookie popup to load
+    time.sleep(5)
 
-# Input user input into search
-searchInput1 = driver.find_element_by_xpath('//*[@id="what"]')
-searchInput1.send_keys(companyName)
+    # Accept cookie popup
+    cookiesHandle = driver.find_element_by_xpath(
+        '//*[@id="CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"]'
+    )
+    cookiesHandle.click()
 
-searchInput2 = driver.find_element_by_xpath('//*[@id="where"]')
-searchInput2.send_keys(location)
+    # Input user input into search
+    searchInput1 = driver.find_element_by_xpath('//*[@id="what"]')
+    searchInput1.send_keys(companytype)
 
-sendInput = driver.find_element_by_xpath('//*[@id="btnSearch"]')
-sendInput.click()
+    searchInput2 = driver.find_element_by_xpath('//*[@id="where"]')
+    searchInput2.send_keys(location)
 
-nameList = driver.find_elements_by_xpath(
-    '//*[@id="main-container"]/div[1]/div[1]/ul[3]/li/div/a/h2'
-)
+    sendInput = driver.find_element_by_xpath('//*[@id="btnSearch"]')
+    sendInput.click()
 
-listOfLinks = []
-condition = True
+    nameList = driver.find_elements_by_xpath(
+        '//*[@id="main-container"]/div[1]/div[1]/ul[3]/li/div/a/h2'
+    )
 
-m = 0
+    listOfLinks = []
+    condition = True
 
-print("Obtaining links...")
-while condition:
-    CompanyList = driver.find_elements_by_xpath('//a[@itemprop="url"]')
-    s = 0
-    for el in CompanyList:
-        listOfLinks.append(el.get_property("href"))
-        s += 1
-    try:
-        if driver.find_element_by_class_name("next").get_property("href") != "":
-            driver.find_element_by_class_name("next").click()
-        else:
-            condition = False
-    except:
-        condition = False
+    m = 0
 
-    m += 1
-
-alldetails = []
-
-test = 0
-
-print("Extracting data...")
-for i in tqdm(listOfLinks):
-    # print("iterating")
-    driver.get(i)
-    temp = {}
-    temp["Firma/Suchbegriff"] = companyName
-    temp["Bezirk/Ort/Plz"] = location
-    try:
-        name = driver.find_element_by_xpath('//div[@itemprop="name"]').text
-        temp["Firmenname"] = name
-    except:
-        temp["Firmenname"] = ""
-        # print('error en name')
-    try:
-        streetAdress = driver.find_element_by_xpath(
-            '//span[@itemprop="streetAddress"]'
-        ).text
-        temp["Straße"] = streetAdress
-    except:
-        temp["Straße"] = ""
-        # print('error in streeAddress')
-    try:
-        postalCode = driver.find_element_by_xpath('//span[@itemprop="postalCode"]').text
-        temp["PLZ"] = postalCode
-    except:
-        temp["PLZ"] = ""
-        # print('error in postalcode')
-    try:
-        addressLocality = driver.find_element_by_xpath(
-            '//span[@itemprop="addressLocality"]'
-        ).text
-        temp["Stadt"] = addressLocality
-    except:
-        temp["Stadt"] = ""
-        # print('error in addressLocality')
-    try:
-        telephones = driver.find_elements_by_xpath('//span[@itemprop="telephone"]')
-        texts = [i.text for i in telephones]
+    print("Obtaining links...")
+    while condition:
+        CompanyList = driver.find_elements_by_xpath('//a[@itemprop="url"]')
+        s = 0
+        for el in CompanyList:
+            listOfLinks.append(el.get_property("href"))
+            s += 1
         try:
-            temp["Tel_1"] = texts[0]
+            if driver.find_element_by_class_name("next").get_property("href") != "":
+                driver.find_element_by_class_name("next").click()
+            else:
+                condition = False
+        except:
+            condition = False
+
+        m += 1
+
+    alldetails = []
+
+    test = 0
+
+    print("Extracting data...")
+    for i in tqdm(listOfLinks):
+        # print("iterating")
+        driver.get(i)
+        temp = {}
+        temp["Firma/Suchbegriff"] = companytype
+        temp["Bezirk/Ort/Plz"] = location
+        try:
+            name = driver.find_element_by_xpath('//div[@itemprop="name"]').text
+            temp["Firmenname"] = name
+        except:
+            temp["Firmenname"] = ""
+            # print('error en name')
+        try:
+            streetAdress = driver.find_element_by_xpath(
+                '//span[@itemprop="streetAddress"]'
+            ).text
+            temp["Straße"] = streetAdress
+        except:
+            temp["Straße"] = ""
+            # print('error in streeAddress')
+        try:
+            postalCode = driver.find_element_by_xpath('//span[@itemprop="postalCode"]').text
+            temp["PLZ"] = postalCode
+        except:
+            temp["PLZ"] = ""
+            # print('error in postalcode')
+        try:
+            addressLocality = driver.find_element_by_xpath(
+                '//span[@itemprop="addressLocality"]'
+            ).text
+            temp["Stadt"] = addressLocality
+        except:
+            temp["Stadt"] = ""
+            # print('error in addressLocality')
+        try:
+            telephones = driver.find_elements_by_xpath('//span[@itemprop="telephone"]')
+            texts = [i.text for i in telephones]
+            try:
+                temp["Tel_1"] = texts[0]
+            except:
+                temp["Tel_1"] = ""
+            try:
+                temp["Tel_2"] = texts[1]
+            except:
+                temp["Tel_2"] = ""
         except:
             temp["Tel_1"] = ""
-        try:
-            temp["Tel_2"] = texts[1]
-        except:
             temp["Tel_2"] = ""
-    except:
-        temp["Tel_1"] = ""
-        temp["Tel_2"] = ""
-    try:
-        faxNumber = driver.find_element_by_xpath('//span[@itemprop="faxNumber"]').text
-        temp["Fax"] = faxNumber
-    except:
-        temp["Fax"] = ""
-        # print('error in faxnomber')
-    try:
-        email = driver.find_element_by_xpath(
-            '//*[@id="main-container"]/div/div[1]/div[2]/div[1]/div[1]/div[2]/div[2]/div/div/a'
-        ).text
-        temp["Mail - 1"] = email
-    except:
-        temp["Mail - 1"] = ""
-        # print('error en email')
-    try:
-        web = driver.find_element_by_xpath('//a[@itemprop="url"]').text
-        temp["Web"] = web
-    except:
-        temp["Web"] = ""
-        # print('error en web')
+        try:
+            faxNumber = driver.find_element_by_xpath('//span[@itemprop="faxNumber"]').text
+            temp["Fax"] = faxNumber
+        except:
+            temp["Fax"] = ""
+            # print('error in faxnomber')
+        try:
+            email = driver.find_element_by_xpath(
+                '//*[@id="main-container"]/div/div[1]/div[2]/div[1]/div[1]/div[2]/div[2]/div/div/a'
+            ).text
+            temp["Mail - 1"] = email
+        except:
+            temp["Mail - 1"] = ""
+            # print('error en email')
+        try:
+            web = driver.find_element_by_xpath('//a[@itemprop="url"]').text
+            temp["Web"] = web
+        except:
+            temp["Web"] = ""
+            # print('error en web')
 
-    ##### CREDIT INFO ####
-    # Descripcion del puesto
-    try:
-        descripcionPuesto = driver.find_element_by_xpath(
-            '//span[@itemprop="description"]'
-        ).text
-        temp["Tätigkeitsbeschreibung - 1"] = descripcionPuesto
-    except:
-        temp["Tätigkeitsbeschreibung - 1"] = ""
-        # print("error en descripcionPuesto")
+        ##### CREDIT INFO ####
+        # Descripcion del puesto
+        try:
+            descripcionPuesto = driver.find_element_by_xpath(
+                '//span[@itemprop="description"]'
+            ).text
+            temp["Tätigkeitsbeschreibung - 1"] = descripcionPuesto
+        except:
+            temp["Tätigkeitsbeschreibung - 1"] = ""
+            # print("error en descripcionPuesto")
 
-    # Numero UUUD
-    try:
-        numeroUID = driver.find_element_by_xpath('//span[@itemprop="vatID"]').text
-        temp["UID-Nummer:"] = numeroUID
-    except:
-        temp["UID-Nummer:"] = ""
-        # print("error en numeroUID")
+        # Numero UUUD
+        try:
+            numeroUID = driver.find_element_by_xpath('//span[@itemprop="vatID"]').text
+            temp["UID-Nummer:"] = numeroUID
+        except:
+            temp["UID-Nummer:"] = ""
+            # print("error en numeroUID")
 
-    # Fecha de inicio de forma juridica
-    try:
-        fechaInicioFormaJuridica = driver.find_element_by_xpath(
-            '//span[@itemprop="foundingDate"]'
-        ).text
-        temp["Beginndatum der Rechtsform:"] = fechaInicioFormaJuridica
-    except:
-        temp["Beginndatum der Rechtsform:"] = ""
-        # print("error en fechaInicioFormaJuridica")
-    ############ extracting Haldelnde personen ############
-    try:
-        i = 0
-        condition = True
-        while condition:
-            accionistas = driver.find_elements_by_xpath(
-                '//*[@id="crefo"]/div[2]/div[{0}]'.format(5 - i)
-            )
-            list_of_strings = []
-            for a in accionistas:
-                b = a.text
-                # print(b)
-                if b == "" and b.isspace():
-                    print(".")
-                else:
-                    list_of_strings.append(b)
-            list_of_strings = list_of_strings[0].split("\n\n")
+        # Fecha de inicio de forma juridica
+        try:
+            fechaInicioFormaJuridica = driver.find_element_by_xpath(
+                '//span[@itemprop="foundingDate"]'
+            ).text
+            temp["Beginndatum der Rechtsform:"] = fechaInicioFormaJuridica
+        except:
+            temp["Beginndatum der Rechtsform:"] = ""
+            # print("error en fechaInicioFormaJuridica")
+        ############ extracting Haldelnde personen ############
+        try:
+            i = 0
+            condition = True
+            while condition:
+                accionistas = driver.find_elements_by_xpath(
+                    '//*[@id="crefo"]/div[2]/div[{0}]'.format(5 - i)
+                )
+                list_of_strings = []
+                for a in accionistas:
+                    b = a.text
+                    # print(b)
+                    if b == "" and b.isspace():
+                        print(".")
+                    else:
+                        list_of_strings.append(b)
+                list_of_strings = list_of_strings[0].split("\n\n")
 
-            if "Handelnde Personen:" in list_of_strings:
-                list_acc = []
-                prove = []
-                proveAnteil = []
-                for item in list_of_strings[1:]:
-                    dictjsn = {}
-                    accionistas = item.split("\n")
-                    if len(accionistas) > 1:
-                        prove.append(accionistas[0])
-                        n = prove.count(accionistas[0])
-                        temp[accionistas[0] + " - " + str(n)] = accionistas[1]
-                        if "Anteil" in accionistas[3]:
-                            temp["Anteil" + " - " + str(n)] = accionistas[3][7:]
-                        list_acc.append(dictjsn)
+                if "Handelnde Personen:" in list_of_strings:
+                    list_acc = []
+                    prove = []
+                    proveAnteil = []
+                    for item in list_of_strings[1:]:
+                        dictjsn = {}
+                        accionistas = item.split("\n")
+                        if len(accionistas) > 1:
+                            prove.append(accionistas[0])
+                            n = prove.count(accionistas[0])
+                            temp[accionistas[0] + " - " + str(n)] = accionistas[1]
+                            if "Anteil" in accionistas[3]:
+                                temp["Anteil" + " - " + str(n)] = accionistas[3][7:]
+                            list_acc.append(dictjsn)
 
-                        # temp["accionistas"] = list_acc
-                    if len(accionistas) == 1:
-                        temp[accionistas[0] + " - " + str(1)] = accionistas[1:]
+                            # temp["accionistas"] = list_acc
+                        if len(accionistas) == 1:
+                            temp[accionistas[0] + " - " + str(1)] = accionistas[1:]
 
-                condition = False
-            i += 1
-    except:
-        continue
+                    condition = False
+                i += 1
+        except:
+            continue
 
-    test += 1
+        test += 1
 
-    alldetails.append(temp)
-    if test == int(len(listOfLinks) * limit / 100):
-        break
+        alldetails.append(temp)
+        if test == int(len(listOfLinks) * limit / 100):
+            break
 
-# converting to table format
-df = pd.DataFrame(alldetails)
-print(df)  # visualizacion
-# exporting table in excel format
-df.to_excel(companyName + "_" + location + ".xlsx")
+    # Converting to table format
+    df = pd.DataFrame(alldetails)
+    print(df)  # visualizacion
+    # Exporting table in excel format
+    filename = companytype + "_" + location
+    df.to_excel("leads/" + filename + ".xlsx")
 
-driver.close()
+    driver.close()
+
+    return filename
+
+scrape_firmenabc()
