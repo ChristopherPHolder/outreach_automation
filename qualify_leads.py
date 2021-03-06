45b8666eb85c0bfd0bf21caacfb319041b77e118
Christopher <toholderandrews@gmail.com>
2021-03-06 14:25:26 -0600

    structured the code so that it can be called as an independent fn and feed inputs

--- qualify_leads.py
+++ qualify_leads.py
@@ -8,130 +8,134 @@ from tqdm import tqdm
 
 from comp.get_user_input import get_wordlist_ql, get_leadsfile_ql
 
-wordlist = get_wordlist_ql()
-# This fuction provides the name of the funtion without the path to add
-leadsfile = get_leadsfile_ql()
+def qualify_leads_fn(filename, wordlist):
 
-# Open leadsfile
-df = pd.read_excel("leads/" + leadsfile)
+    # Open leadsfile
+    df = pd.read_excel("leads/" + filename)
 
-# Filtering companies with web
-df_web = df[pd.notnull(df["Web"])].reset_index(drop=True)
+    # Filtering companies with web
+    df_web = df[pd.notnull(df["Web"])].reset_index(drop=True)
 
-# Extract subdomains to make the first filter
-subdomains = wordlist[wordlist["State"] == "Subdomains"].Word.tolist()
+    # Extract subdomains to make the first filter
+    subdomains = wordlist[wordlist["State"] == "Subdomains"].Word.tolist()
 
-# Total of rows in database
-total_rows = df_web.shape[0]
-counter = 0
-list_of_urls = df_web.Web.tolist()
-totally = []
-totally_content = []
-for url in list_of_urls:
-    print("<--------------------------------------------------> ", counter + 1)
-    # Extract all links of the page
-    try:
-        url = "https://" + url
-        print("Result for: ", url)
-        req = requests.get(url, timeout=5)
-        print(req.status_code)
-        if req.status_code != 200:
-            print("Error obteniendo la página. Status Code", r.status_code)
-        soup = BeautifulSoup(req.text, "lxml")
-        all_a_s = soup.find_all("a")
-    except Exception as e:
-        print("Error obtaining page. Exception error:\n", e)
-    try:
-        list_of_urls_by_page = []
-        for item in all_a_s:
-            if item is not None:
-                try:
-                    list_of_urls_by_page.append(item.get("href"))
-                except:
-                    pass
-    except:
-        pass
-    # Filtrate links that contain key words
-    try:
-        total_sublist = []
-        for url_single in list_of_urls_by_page:
-            lists = [word for word in subdomains if word.lower() in url_single.lower()]
-            if len(lists) >= 1:
-                total_sublist.append(url_single)
-    except:
-        total_sublist = []
-    totally.append(total_sublist)
-    counter += 1
-
-    if counter == total_rows:
-        break
-
-list_total = []
-
-# Initializing new column
-df_web.loc[:, "qualify"] = np.nan
-
-# Extract labels for classification
-types = wordlist.State.unique().tolist()
-types.remove("Subdomains")
-
-index = 0
-ordered = []
-for list_items in totally:
-    classifications_of_all_links_in_page = []
-    for element in list_items:
+    # Total of rows in database
+    total_rows = df_web.shape[0]
+    counter = 0
+    list_of_urls = df_web.Web.tolist()
+    totally = []
+    totally_content = []
+    for url in list_of_urls:
+        print("<--------------------------------------------------> ", counter + 1)
+        # Extract all links of the page
         try:
-            rr = requests.get(element)
-            ss = BeautifulSoup(rr.text, "lxml")
-            if ss:
-                print(index + "successful request")
+            url = "https://" + url
+            print("Result for: ", url)
+            req = requests.get(url, timeout=5)
+            print(req.status_code)
+            if req.status_code != 200:
+                print("Error obteniendo la página. Status Code", r.status_code)
+            soup = BeautifulSoup(req.text, "lxml")
+            all_a_s = soup.find_all("a")
         except Exception as e:
+            print("Error obtaining page. Exception error:\n", e)
+        try:
+            list_of_urls_by_page = []
+            for item in all_a_s:
+                if item is not None:
+                    try:
+                        list_of_urls_by_page.append(item.get("href"))
+                    except:
+                        pass
+        except:
+            pass
+        # Filtrate links that contain key words
+        try:
+            total_sublist = []
+            for url_single in list_of_urls_by_page:
+                lists = [word for word in subdomains if word.lower() in url_single.lower()]
+                if len(lists) >= 1:
+                    total_sublist.append(url_single)
+        except:
+            total_sublist = []
+        totally.append(total_sublist)
+        counter += 1
+
+        if counter == total_rows:
+            break
+
+    list_total = []
+
+    # Initializing new column
+    df_web.loc[:, "qualify"] = np.nan
+
+    # Extract labels for classification
+    types = wordlist.State.unique().tolist()
+    types.remove("Subdomains")
+
+    index = 0
+    ordered = []
+    for list_items in totally:
+        classifications_of_all_links_in_page = []
+        for element in list_items:
             try:
-                new_url = "https://" + df_web.loc[index, "Web"] + element
-                rr = requests.get(new_url)
+                rr = requests.get(element)
                 ss = BeautifulSoup(rr.text, "lxml")
-            except:
-                ss = ""
-        classification = []
-        for tp in types:
-            list_of_words_to_match = wordlist[wordlist["State"] == tp].Word.tolist()
-            words_founded = [
-                word
-                for word in list_of_words_to_match
-                if word.lower() in str(ss).lower()
-            ]
-            if len(words_founded) >= 1:
-                classification.append(tp)
-        classifications_of_all_links_in_page.append(classification)
-    ordered.append(classifications_of_all_links_in_page)
-    index += 1
+                if ss:
+                    print(index + "successful request")
+            except Exception as e:
+                try:
+                    new_url = "https://" + df_web.loc[index, "Web"] + element
+                    rr = requests.get(new_url)
+                    ss = BeautifulSoup(rr.text, "lxml")
+                except:
+                    ss = ""
+            classification = []
+            for tp in types:
+                list_of_words_to_match = wordlist[wordlist["State"] == tp].Word.tolist()
+                words_founded = [
+                    word
+                    for word in list_of_words_to_match
+                    if word.lower() in str(ss).lower()
+                ]
+                if len(words_founded) >= 1:
+                    classification.append(tp)
+            classifications_of_all_links_in_page.append(classification)
+        ordered.append(classifications_of_all_links_in_page)
+        index += 1
 
 
-def priorities(list_of_items):
-    qualifying = []
-    for items in list_of_items:
-        total_links = []
-        for item in items:
-            total_links.append(len(item))
-            if (("No" in item) and ("Fuck Yes" in item) and ("Yes" in item)) or (("No" in item) and ("Fuck Yes" in item)) or (("No" in item) and ("Yes" in item)):
-                status = "No"
-            elif (
-                ("No" in item)
-                and ("Fuck Yes" in item)
-                and ("Yes" in item)
-                and ("Maybe" in item)
-            ):
-                status = "Maybe"
-            elif ("No" in item) and ("Maybe" in item):
-                status = 'Maybe'
-            else:
-                for tt in item:
-                    status = tt
-        if sum(total_links) == 0:
-            status = "Fuck No"
-        qualifying.append(status)
-    return qualifying
+    def priorities(list_of_items):
+        qualifying = []
+        for items in list_of_items:
+            total_links = []
+            for item in items:
+                total_links.append(len(item))
+                if (("No" in item) and ("Fuck Yes" in item) and ("Yes" in item)) or (("No" in item) and ("Fuck Yes" in item)) or (("No" in item) and ("Yes" in item)):
+                    status = "No"
+                elif (
+                    ("No" in item)
+                    and ("Fuck Yes" in item)
+                    and ("Yes" in item)
+                    and ("Maybe" in item)
+                ):
+                    status = "Maybe"
+                elif ("No" in item) and ("Maybe" in item):
+                    status = 'Maybe'
+                else:
+                    for tt in item:
+                        status = tt
+            if sum(total_links) == 0:
+                status = "Fuck No"
+            qualifying.append(status)
+        return qualifying
 
+    df_web.loc[:, "qualify"] = priorities(ordered)
+    filename = filename + "_qualified"
+    df_web.to_excel("leads/" + filename + ".xlsx")
 
-df_web.loc[:, "qualify"] = priorities(ordered)
-df_web.to_excel("leads/" + "qualified_" + leadsfile)
+    return filename
+
+wordlist = get_wordlist_ql()
+filename = get_leadsfile_ql()
+qualify_leads_fn(filename, wordlist)
