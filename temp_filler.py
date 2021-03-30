766b8f9d06b64a384ffbf620dc54673cdc8d75c1
Christopher <toholderandrews@gmail.com>
2021-03-30 14:44:01 -0600

    Template takes in a file name and extracts the bosses and splits the relevant parts into a dict

--- temp_filler.py
+++ temp_filler.py
@@ -1,17 +1,48 @@
+from docxtpl import DocxTemplate
 import pandas as pd
 
-def extract_company_name(data):
-    c_name = pd.DataFrame(data, columns= ['Firmenname'])
-    return c_name
-
-def extract_company_address(data):
+def fill_temp(filename):
+    df = pd.read_excel("leads/" + filename + '_clean' + ".xlsx") # Opens excel file into dataframe
+    bosses = split_name(df)
+    print(bosses)
+    doc = DocxTemplate("word_templates/cold_postmail_1.docx")
+    context = {
+        'title' : "Herr",
+        'last_name': 'Holder'
+        }
+    doc.render(context)
+    doc.save("generated_doc.docx")
     return 0
 
-def extract_bosses_name(data):
-    return 0
+def split_name(df):
+    bosses = [] # Iniciates a list
+    for i in df.index: 
+        x = df['Geschäftsführer'][i].split()
+        if x[0] == 'Herr' or x[0] == 'Frau':
+            #print('title: ' + x[0])
+            if x[1].endswith('.') == False:
+                #print('last_name: ' + x[1])
+                if x[len(x) - 1].endswith('.') == False:
+                    bosses.append({
+                        'id': i,
+                        'title': x[0], 
+                        'last_name': x[1], 
+                        'first_name': x[len(x) - 1]
+                        })
+                    #print('first_name: ' + x[len(x) - 1])
 
-def extract_lead_data(data):
-    c_name = extract_company_name(data)
-    c_address = extract_company_address(data)
-    c_boss = extract_bosses_name(data)
-    return 0
+            elif x[2].endswith('.') == False:
+                #print('last_name: ' + x[2])
+                if x[len(x) - 1].endswith('.') == False: 
+                    #print('first_name: ' + x[len(x) - 1])
+                    bosses.append({
+                        'id': i,
+                        'title': x[0], 
+                        'last_name': x[2], 
+                        'first_name': x[len(x) - 1]
+                        })
+    
+    for boss in bosses: 
+        print(boss)
+    
+    return bosses
