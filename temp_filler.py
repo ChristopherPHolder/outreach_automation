0999d5898caab5022a013e5d26c9f7ee9d67e475
Christopher <toholderandrews@gmail.com>
2021-03-30 15:32:17 -0600

    Creates a template per boss and stores them in a pending_post/ as well as links them to the row they are in on the index

--- temp_filler.py
+++ temp_filler.py
@@ -3,46 +3,37 @@ import pandas as pd
 
 def fill_temp(filename):
     df = pd.read_excel("leads/" + filename + '_clean' + ".xlsx") # Opens excel file into dataframe
-    bosses = split_name(df)
-    print(bosses)
-    doc = DocxTemplate("word_templates/cold_postmail_1.docx")
-    context = {
-        'title' : "Herr",
-        'last_name': 'Holder'
-        }
-    doc.render(context)
-    doc.save("generated_doc.docx")
-    return 0
+    bosses = split_name(df) # Gets list of bosses and there information
 
+    # Iterates over each boss and creates word file
+    for boss in bosses: # Itereates over list of bosses
+        doc = DocxTemplate("word_templates/cold_postmail_1.docx") # Open template
+        doc.render(boss) # Introduces data into template 
+        doc.save("pending_post/postmail" + str(boss['id']) + ".docx") # Creates new word file
+
+# Creates a list of dicts with the information of the bosses
 def split_name(df):
-    bosses = [] # Iniciates a list
-    for i in df.index: 
-        x = df['Geschäftsführer'][i].split()
-        if x[0] == 'Herr' or x[0] == 'Frau':
-            #print('title: ' + x[0])
-            if x[1].endswith('.') == False:
-                #print('last_name: ' + x[1])
-                if x[len(x) - 1].endswith('.') == False:
+    bosses = [] # Iniciates an empty list
+    for i in df.index: # Loops over each row in the leads file
+        x = df['Geschäftsführer'][i].split() # Splits the bosses name into a list of strings
+        if x[0] == 'Herr' or x[0] == 'Frau': # If the first word is a personal title
+            if x[1].endswith('.') == False: # If the does not end with a '.' (proficional titles en with a '.')
+                if x[len(x) - 1].endswith('.') == False: # If last word does not end with '.' 
+                    # Adds boss to list of bosses
                     bosses.append({
                         'id': i,
                         'title': x[0], 
                         'last_name': x[1], 
                         'first_name': x[len(x) - 1]
                         })
-                    #print('first_name: ' + x[len(x) - 1])
 
-            elif x[2].endswith('.') == False:
-                #print('last_name: ' + x[2])
-                if x[len(x) - 1].endswith('.') == False: 
-                    #print('first_name: ' + x[len(x) - 1])
+            elif x[2].endswith('.') == False: # Since 2nd word ened in '.' check 3rd
+                if x[len(x) - 1].endswith('.') == False: # Check if last word ends in '.'
+                    # Adds boss to list of bosses
                     bosses.append({
                         'id': i,
                         'title': x[0], 
                         'last_name': x[2], 
                         'first_name': x[len(x) - 1]
                         })
-    
-    for boss in bosses: 
-        print(boss)
-    
     return bosses
