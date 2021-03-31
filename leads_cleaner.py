f7674846fd345074e112fb59135a6f006d5457eb
Christopher <toholderandrews@gmail.com>
2021-03-31 12:52:02 -0600

    Fixed bugs related to titles and emptry bosses

--- leads_cleaner.py
+++ leads_cleaner.py
@@ -18,22 +18,30 @@ def find_boss(df):
         ]
     # Iterate over the leads from the excel file
     for i in df.index:
+        print("Index iteration: " + str(i))
         # Check is lead doesnt have a clear boss
         if pd.isnull(df['Geschäftsführer - 1'][i]) == True:
+            print("Geschäftsführer - 1 is emptry for " + df['Firmenname'][i])
             # Check if it has a boss any of the other fields
-            loopbreaker = 0 # Incase lead has no boss at all create a infinate loop breaker. 
-            while pd.isnull(df['Geschäftsführer - 1'][i]) == True or loopbreaker > 100: # Run until it finds a boss or 100 iterations
+            loopbreaker = 0 # Incase lead has no boss at all create a infinate loop breaker.
+            print("Memory safety: " + str(loopbreaker))
+            while pd.isnull(df['Geschäftsführer - 1'][i]) == True and loopbreaker < 100: # Run until it finds a boss or 100 iterations
                 loopbreaker += 1 # Safegard agains infinate loops
+                print("Memory safety: " + str(loopbreaker))
                 for z in priorities: # Goes over the priorities 
                     for x in range(1, 20): # Checks up to 18 priority termination numbers
                         try: 
                             boss = z + ' - ' + str(x) # Extracts boss name/title as in excel sheet
+                            print("Checking for: " + boss)
                             if pd.isnull(df[boss][i]) == False: # Checks if cell is emprty or if boss is name is there
                                 x = df[boss][i].split() # Split field into a list
+                                print("Boss found as: " + boss)
                                 if x[0] != 'Firma': # Check that first word is
+                                    print(x[0])
                                     df['Geschäftsführer - 1'][i] = df[boss][i] # Adds the boss is name as boss 1
                         except KeyError: # In case boss title is out of range
                             break
+    print("Fining boss process is completed")
     return df
 
 def remove_extra_bosses(df):
@@ -49,8 +57,9 @@ def remove_extra_bosses(df):
         'Geschäftsführer', 
         'Bezirk/Ort/Plz', 
         'Firmenname', 
-        'Straße', 
-        'Stadt', 
+        'Straße',
+        'PLZ', 
+        'Stadt',
         'Tel', 
         'Mail', 
         'Web']
@@ -59,5 +68,11 @@ def remove_extra_bosses(df):
     for i in df.columns:
         if i not in imp_info:
             df = df.drop(columns=i)
+
+    for i in df.index:
+        if pd.isnull(df['Geschäftsführer'][i]):
+            print("Droping empty row: " + str(df.index[i]))
+            print("Company dropped was: " + str(df['Firmenname'][i]))
+            df = df.drop(df.index[i])
 
     return df
--- temp_filler.py
+++ temp_filler.py
@@ -9,17 +9,20 @@ def fill_temp(filename):
     for boss in bosses: # Itereates over list of bosses
         doc = DocxTemplate("word_templates/cold_postmail_1.docx") # Open template
         doc.render(boss) # Introduces data into template 
-        doc.save("pending_post/postmail_" + filename + str(boss['_id']) + ".docx") # Creates new word file
+        doc.save("pending_post/postmail_" + filename + "_" + str(boss['id']) + ".docx") # Creates new word file
 
     
 
 # Creates a list of dicts with the information of the bosses
 def split_name(df):
     bosses = [] # Iniciates an empty list
+    prof_title = ['(FH)', 'Dipl.-', 'MBA', 'BSc']
     for i in df.index: # Loops over each row in the leads file
+        print("Template index is: " + str(i))
         x = df['Geschäftsführer'][i].split() # Splits the bosses name into a list of strings
         if x[0] == 'Herr' or x[0] == 'Frau': # If the first word is a personal title
-            if x[1].endswith('.') == False: # If the does not end with a '.' (proficional titles en with a '.')
+            if x[1].endswith('.') == False and x[1] not in prof_title: # If the does not end with a '.' (proficional titles en with a '.')
+                print("Last name: " + x[1])
                 if x[len(x) - 1].endswith('.') == False: # If last word does not end with '.' 
                     # Adds boss to list of bosses
                     bosses.append({
@@ -29,7 +32,8 @@ def split_name(df):
                         'first_name': x[len(x) - 1]
                         })
 
-            elif x[2].endswith('.') == False: # Since 2nd word ened in '.' check 3rd
+            elif x[2].endswith('.') == False and x[2] not in prof_title: # Since 2nd word ened in '.' check 3rd
+                print("Last name: " + x[2])
                 if x[len(x) - 1].endswith('.') == False: # Check if last word ends in '.'
                     # Adds boss to list of bosses
                     bosses.append({
@@ -38,4 +42,18 @@ def split_name(df):
                         'last_name': x[2], 
                         'first_name': x[len(x) - 1]
                         })
+
+            elif x[3].endswith('.') == False and x[3] not in prof_title: # Since 2nd word ened in '.' check 3rd
+                print("Last name: " + x[3])
+                if x[len(x) - 1].endswith('.') == False: # Check if last word ends in '.'
+                    # Adds boss to list of bosses
+                    bosses.append({
+                        'id': i,
+                        'title': x[0], 
+                        'last_name': x[3], 
+                        'first_name': x[len(x) - 1]
+                        })
+
+    for boss in bosses:
+        print(boss['last_name'])
     return bosses
