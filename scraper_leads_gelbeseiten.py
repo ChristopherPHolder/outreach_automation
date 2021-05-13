2355f0e628ce2a9ae7829cea0070f80e2f198b67
Christopher <toholderandrews@gmail.com>
2021-05-13 14:26:24 +0200

    Created some additional error handling

--- scraper_leads_gelbeseiten.py
+++ scraper_leads_gelbeseiten.py
@@ -11,6 +11,7 @@ from selenium.webdriver.support import expected_conditions
 
 import time  # Library to allow waiting and sleep
 import re # Library for resplting data
+import pandas as pd # Library to store and export data formated as a table
 
 def scrape_gelbesieten():
     driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -33,24 +34,23 @@ def scrape_gelbesieten():
     search_input_was = driver.find_element_by_xpath(
         '//*[@id="what_search"]'
     )
-    search_input_was.send_keys('steuerberater')
+    search_input_was.send_keys('Steuerberater')
 
     search_input_wo = driver.find_element_by_xpath(
         '//*[@id="where_search"]'
     )
-    search_input_wo.send_keys('berlin')
+    search_input_wo.send_keys('Munster')
 
     send_input_button = driver.find_element_by_xpath(
         '//*[@id="transform_wrapper"]/section/div/div/div/div[1]\
         /div/div/form/div/div[2]/button'
     )
     send_input_button.click()
-    
+   
     # Change range to 50 km
     search_radius_range_input = driver.find_element_by_xpath(
         '//*[@id="suchradius_slider"]'
     )
-
     search_radius_range_input_id = 'suchradius_slider'
     ignored_exceptions = (
         NoSuchElementException,
@@ -75,6 +75,9 @@ def scrape_gelbesieten():
         '//*[@id="loadMoreGezeigteAnzahl"]'
     ).text
     initial_time = time.perf_counter()
+
+    time.sleep(15) # Allows banner add to move out the way
+
     while True:
         current_list_size = driver.find_element_by_xpath(
             '//*[@id="loadMoreGezeigteAnzahl"]'
@@ -105,12 +108,12 @@ def scrape_gelbesieten():
     tel = []
     mail = []
     web = []
+    nan = float('NaN')
+    for lead in range(int(current_list_size)):
+        geschaftsfuhrer.append(nan)
 
-    for lead in range(current_list_size):
-        geschaftsfuhrer.append(NaN = float('NaN'))
-        
         address = driver.find_element_by_xpath(
-            f'//*[@id="gs_treffer"]/div/article[{lead}]/a/address/p[1]'
+            f'//*[@id="gs_treffer"]/div/article[{lead+1}]/a/address/p[1]'
         ).text
         address_split = re.split(',|\(|\)', address)
         address_re_split = address_split[1].split(' ')
@@ -119,36 +122,41 @@ def scrape_gelbesieten():
 
         firmenname.append(
             driver.find_element_by_xpath(
-                f'//*[@id="gs_treffer"]/div/article[{lead}]/a/h2'
+                f'//*[@id="gs_treffer"]/div/article[{lead+1}]/a/h2'
             ).text
         )
-
         strasse.append(address_split[0])
 
         plz.append(address_re_split[1])
 
         stadt.append(address_re_split[2])
-
-        tel.append(
-            driver.find_element_by_xpath(
-                f'//*[@id="gs_treffer"]/div/article[{lead}]/a/address/p[2]'
-            ).text
-        )
-
-        mail_link = driver.find_element_by_xpath(
-            f'//*[@id="gs_treffer"]/div/article[{lead}]/div/div/a[2]'
-        ).get_attribute('href')
-        mail_link_re_split = re.split('\:|\?', mail_link)
-        mail.append(mail_link_re_split[1])
-
-        web.append(
-            driver.find_element_by_xpath(
-                f'//*[@id="gs_treffer"]/div/article[{lead}]/div/div/a[1]'
+        try:
+            tel.append(
+                driver.find_element_by_xpath(
+                    f'//*[@id="gs_treffer"]/div/article[{lead+1}]/a/address/p[2]'
+                ).text
+            )
+        except NoSuchElementException:
+            tel.append(nan)
+        try:
+            mail_link = driver.find_element_by_xpath(
+                f'//*[@id="gs_treffer"]/div/article[{lead+1}]/div/div/a[2]'
             ).get_attribute('href')
-        )
+            mail_link_re_split = re.split('\:|\?', mail_link)
+            mail.append(mail_link_re_split[1])
+        except NoSuchElementException:
+            mail.append(nan)
+        try:
+            web.append(
+                driver.find_element_by_xpath(
+                    f'//*[@id="gs_treffer"]/div/article[{lead}]/div/div/a[1]'
+                ).get_attribute('href')
+            )
+        except NoSuchElementException:
+            web.append(nan)
 
     # Creating DataFrame
-    lead_info = {
+    lead_data = {
         'Geschäftsführer': geschaftsfuhrer, 
         'Bezirk/Ort/Plz': bezirk_ort_plz, 
         'Firmenname': firmenname, 
@@ -159,3 +167,6 @@ def scrape_gelbesieten():
         'Mail': mail, 
         'Web': web,
     }
+
+    df = pd.DataFrame(data=lead_data)
+    print(df)
