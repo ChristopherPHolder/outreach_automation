1bbbd13318155f6b9273783807a7a9df1b4d8eee
Christopher <toholderandrews@gmail.com>
2021-05-19 23:03:32 +0200

    feat: add counter of number of times the managing directors title is found in the imprint and progress bars to show user how much time is left

--- scraper_managing_director.py
+++ scraper_managing_director.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+from tqdm import tqdm
+
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,30 +10,40 @@ from selenium.common.exceptions import WebDriverException,\
                                         StaleElementReferenceException
 
 def add_managing_director(filename):
-    
     df = pd.read_excel("leads/" + filename + ".xlsx")
+
     df = check_add_imprint_column(df)
     options = Options()    
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(2.5)
     df = add_imprint(df, driver)
+
+    df = check_add_imprint_manager_count_column(df)
+    df = add_imprint_manager_count(df, df_g, driver)
+
     driver.close()
     return df
 
 def add_imprint(df, driver):
     n = 0
-    for i in df.index:
-        if pd.isnull(df['Geschäftsführer'][i]) == True\
-        and pd.isnull(df['Web'][i]) == False:
-            n += 1
-            web_url = df['Web'][i]
-            imprint_url = get_imprint_url(web_url, driver)
-            df['Impressum'][i] = imprint_url
-            print(n, i, df['Web'][i], df['Impressum'][i])
+    with tqdm(
+        total = (df.loc[(df['Geschäftsführer'].isnull()) & (df['Impressum'].isnull()), 'Web'].count()),
+        desc='Extracting Imprint'
+    ) as pbar:
+
+        for i in df.index:
+            if pd.isnull(df['Geschäftsführer'][i]) == True\
+            and pd.isnull(df['Web'][i]) == False\
+            and pd.isnull(df['Impressum'][i]) == True:
+                pbar.update(1)
+                n += 1
+                web_url = df['Web'][i]
+                imprint_url = get_imprint_url(web_url, driver)
+                df['Impressum'][i] = imprint_url
+                
     return df
 
 def check_add_imprint_column(df):
-    print(list(df))
     if 'Impressum' not in list(df):
         df['Impressum'] = np.nan
     return df
@@ -41,7 +53,6 @@ def get_imprint_url(web_url, driver):
         driver.get(web_url)
         elems = driver.find_elements_by_xpath("//a[@href]")
     except WebDriverException:
-        print('Page down -->', web_url)
         elems = []
     
     for elem in elems:
@@ -51,17 +62,39 @@ def get_imprint_url(web_url, driver):
         except StaleElementReferenceException:
             pass
     return np.nan
-                
-# Open file as pd
 
-# Add imprint urls
-# Iterate over each lead
-    # If lead does not have managing director
-    # AND If has website AND no imprint
-        # Open website
-        # Def get_imprint_url
-        # If has imprint
-            # Add to pd
+def check_add_imprint_manager_count_column(df):
+    if 'Impressum Word Count' not in list(df):
+        df['Impressum Word Count'] = np.nan
+    return df
+
+def add_imprint_manager_count(df, driver):
+    manager_titles = get_list_of_manager_titles()
+    with tqdm(
+        total = df.loc[(df['Geschäftsführer'].isnull()) & (df['Impressum Word Count'].isnull()), 'Impressum'].count(),
+        desc='Scraping'
+    ) as pbar:
+
+        for i in df.index:
+            if  pd.isnull(df['Geschäftsführer'][i]) == True\
+            and pd.isnull(df['Impressum'][i]) == False\
+            and pd.isnull(df['Impressum Word Count'][i]) == True:
+                pbar.update(1)
+
+                imprint_url = df['Impressum'][i]
+                
+                driver.get(imprint_url)
+                for title in manager_titles:
+                    elems = driver.find_elements_by_xpath("//*[contains(text(),%s)]" % title)
+
+                    if len(elems) != 0:
+                        df['Impressum Word Count'][i] = len(elems)
+
+    return df
+
+def get_list_of_manager_titles():
+    df_titles = pd.read_excel("wordlist/manager_titles.xlsx")
+    return [df_titles['Manager Title'][i] for i in df_titles.index]
 
 # def add manager names 
 # Iterate over each lead
