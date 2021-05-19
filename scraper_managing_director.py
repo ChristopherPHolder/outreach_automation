3f3b67dad430004b2d65c46aae055ba373c19a54
Christopher <toholderandrews@gmail.com>
2021-05-19 13:08:51 +0200

    adds a new column with imprint on it

--- scraper_managing_director.py
+++ scraper_managing_director.py
@@ -1,22 +1,34 @@
 import pandas as pd
 import numpy as np
 
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import WebDriverException,\
+                                        StaleElementReferenceException
+
 def add_managing_director(filename):
+    
     df = pd.read_excel("leads/" + filename + ".xlsx")
     df = check_add_imprint_column(df)
-    df = add_imprint(df)
-    #print(df)
+    options = Options()    
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver.implicitly_wait(5)
+    df = add_imprint(df, driver)
+    driver.close()
     return df
 
-def add_imprint(df):
+def add_imprint(df, driver):
     n = 0
     for i in df.index:
         if pd.isnull(df['Geschäftsführer'][i]) == True\
         and pd.isnull(df['Web'][i]) == False:
             n += 1
-            # check site for imprint
+            web_url = df['Web'][i]
+            imprint_url = get_imprint_url(web_url, driver)
+            df['Impressum'][i] = imprint_url
             print(n, i, df['Web'][i], df['Impressum'][i])
-            
+    return df
 
 def check_add_imprint_column(df):
     print(list(df))
@@ -24,14 +36,22 @@ def check_add_imprint_column(df):
         df['Impressum'] = np.nan
     return df
 
-def get_imprint_url():
-    pass
-
-
+def get_imprint_url(web_url, driver):
+    try:
+        driver.get(web_url)
+        elems = driver.find_elements_by_xpath("//a[@href]")
+    except WebDriverException:
+        print('Page down -->', web_url)
+        elems = []
+    
+    for elem in elems:
+        try:
+            if 'impressum' in elem.get_attribute('href'):
+                return elem.get_attribute('href')
+        except StaleElementReferenceException:
+            pass
+    return np.nan
                 
-                
-
-
 # Open file as pd
 
 # Add imprint urls
