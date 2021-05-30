90a711c6632f073ee1ec8fcdc5de792930a1e7ac
Christopher <toholderandrews@gmail.com>
2021-05-30 04:38:14 +0200

    feat: add name order option and deal with first name and last name order in name extraction from str
    refactor: improve redablility, style and reduce funtion sizes

--- temp_filler.py
+++ temp_filler.py
@@ -1,60 +1,85 @@
 from docxtpl import DocxTemplate
 import pandas as pd
+from tqdm import tqdm
 
-def fill_temp(filename):
-    df = pd.read_excel("leads/" + filename + ".xlsx") # Opens excel file into dataframe
-    bosses = split_name(df) # Gets list of bosses and there information
+def fill_temp(filename, name_order='tfl'):
+    df = pd.read_excel("leads/%s.xlsx" % filename)
+    bosses = get_bosses_name(df, name_order)
 
-    # Iterates over each boss and creates word file
-    for boss in bosses: # Itereates over list of bosses
-        doc = DocxTemplate("word_templates/cold_postmail_1.docx") # Open template
-        doc.render(boss) # Introduces data into template 
-        doc.save("pending_post/postmail_" + filename + "_" + str(boss['id']) + ".docx") # Creates new word file
+    for boss in tqdm(bosses, desc='Filling templates'):
+        doc = DocxTemplate("word_templates/cold_postmail_2.docx")
+        doc.render(boss)
+        id = str(boss['id'])
+        doc.save("pending_post/postmail_%s_%s.docx" % (filename, id))
 
-    
+def get_bosses_name(df, name_order):
+    bosses = []
+    prof_titles = get_prof_titles()
 
-# Creates a list of dicts with the information of the bosses
-def split_name(df):
-    bosses = [] # Iniciates an empty list
-    prof_title = ['(FH)', 'Dipl.-', 'MBA', 'BSc']
-    for i in df.index: # Loops over each row in the leads file
-        print("Template index is: " + str(i))
-        x = df['Geschäftsführer'][i].split() # Splits the bosses name into a list of strings
-        if x[0] == 'Herr' or x[0] == 'Frau': # If the first word is a personal title
-            if x[1].endswith('.') == False and x[1] not in prof_title: # If the does not end with a '.' (proficional titles en with a '.')
-                print("Last name: " + x[1])
-                if x[len(x) - 1].endswith('.') == False: # If last word does not end with '.' 
-                    # Adds boss to list of bosses
-                    bosses.append({
-                        'id': i,
-                        'title': x[0], 
-                        'last_name': x[1].replace(',', ''), 
-                        'first_name': x[len(x) - 1]
-                        })
-
-            elif x[2].endswith('.') == False and x[2] not in prof_title: # Since 2nd word ened in '.' check 3rd
-                print("Last name: " + x[2])
-                if x[len(x) - 1].endswith('.') == False: # Check if last word ends in '.'
-                    # Adds boss to list of bosses
-                    bosses.append({
-                        'id': i,
-                        'title': x[0], 
-                        'last_name': x[2].replace(',', ''), 
-                        'first_name': x[len(x) - 1]
-                        })
-
-            elif x[3].endswith('.') == False and x[3] not in prof_title: # Since 2nd word ened in '.' check 3rd
-                print("Last name: " + x[3])
-                if x[len(x) - 1].endswith('.') == False: # Check if last word ends in '.'
-                    # Adds boss to list of bosses
-                    bosses.append({
-                        'id': i,
-                        'title': x[0], 
-                        'last_name': x[3], 
-                        'first_name': x[len(x) - 1]
-                        })
-
-    for boss in bosses:
-        print(boss['last_name'])
-    
+    for i in tqdm(df.index, desc='Processing names'):
+        x = df['Geschäftsführer'][i].split()
+        if x[0] == 'Herr' or x[0] == 'Frau':
+            name_struct = get_name_struct(x, prof_titles)
+            boss = get_boss_info(i, x, name_order, name_struct)
+            if boss != None:
+                bosses.append(boss)
     return bosses
+
+def get_name_struct(x, prof_titles):
+    if (x[1].endswith('.') == False and
+    x[1] not in prof_titles and
+    x[len(x) - 1].endswith('.') == False):
+        return 'Gender_Name_Name'
+    elif (x[2].endswith('.') == False and
+    x[2] not in prof_titles and
+    x[len(x) - 1].endswith('.') == False):
+        return 'Gender_NotName_Name_Name'
+    elif (x[3].endswith('.') == False and
+    x[3] not in prof_titles and
+    x[len(x) - 1].endswith('.') == False):
+        return 'Gender_NotName_NotName_Name_Name'
+
+
+def get_boss_info(i, x, name_order, name_struct):
+    boss = {'id': i, 'title': x[0]}
+    if name_struct == 'Gender_Name_Name':
+        return get_boss_with_struct_gnn(boss, x, name_order)
+    elif name_struct == 'Gender_NotName_Name_Name':
+        return get_boss_with_struct_gxnn(boss, x, name_order)
+    elif name_struct == 'Gender_NotName_NotName_Name_Name':
+        return get_boss_with_struct_gxxnn(boss, x, name_order)
+
+def get_boss_with_struct_gnn(boss, x, name_order):
+    if name_order == 'tlf':
+        boss['last_name'] = x[1].replace(',', '')
+        boss['first_name'] = x[len(x) - 1]
+    elif name_order == 'tfl':
+        boss['last_name'] = x[len(x) - 1]
+        boss['first_name'] = x[1].replace(',', '')
+    return boss
+
+def get_boss_with_struct_gxnn(boss, x, name_order):
+    if name_order == 'tlf':
+        boss['last_name'] = x[2].replace(',', '') 
+        boss['first_name'] = x[len(x) - 1]
+    elif name_order == 'tfl':
+        boss['last_name'] = x[len(x) - 1]
+        boss['first_name']= x[2].replace(',', '')
+    return boss
+
+
+def get_boss_with_struct_gxxnn(boss, x, name_order):
+    if name_order == 'tlf':
+        boss['last_name'] = x[3], 
+        boss['first_name'] = x[len(x) - 1]
+    elif name_order == 'tfl':
+        boss['last_name'] = x[len(x) - 1]
+        boss['first_name'] = x[3]
+    return boss
+
+def get_prof_titles():
+    prof_titles = [
+        '(FH)', 'Dipl.-', 'MBA', 
+        'BSc', 'Dr.',
+    ]
+    return prof_titles
